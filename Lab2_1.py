0c46ce7942abb74d053a5a5f393c42f79969a4f3
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-09-29 21:17:25 +0200

    function reads file
    tuples - lists

--- Lab2_1.py
+++ Lab2_1.py
@@ -1,13 +1,34 @@
-def main():
-    filename = input("Enter filename")
-    read_file(filename)
+import sys
 
+# sys argv returns the argument to the program
+def main():
+# saves the arguement to the variable
+    filename =sys.argv[1]
+    read_file(filename)
+# function reads the file and
+## puts data in a list filled with tuples
+# tuples have the required format
 def read_file(filename):
-    print(filename)
-## read the file
+    log_file = open(filename, 'r')
+    list_tuples = []
+    end = False
+    while not end:
+        name = log_file.readline()
+        if not name:
+            end = True
+        else:
+            list_tuples.append((name,log_file.readline()))
+    log_file.close()
+    return list_tuples
+
+
 
 def display_entry(name, message):
+    
+
     print(name, message)
 
-if __name__ == '__main__':
-    main()
+
+
+
+main()
