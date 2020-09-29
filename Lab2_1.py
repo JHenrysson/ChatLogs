c8151f94bc61a548bfab4d72a305b855e089e790
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-09-29 13:45:32 +0200

    set up

---
+++ Lab2_1.py
@@ -0,0 +1,13 @@
+def main():
+    filename = input("Enter filename")
+    read_file(filename)
+
+def read_file(filename):
+    print(filename)
+## read the file
+
+def display_entry(name, message):
+    print(name, message)
+
+if __name__ == '__main__':
+    main()
