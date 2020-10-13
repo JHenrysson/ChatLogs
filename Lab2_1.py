29c32ebede9de623f18d7187b375a1c2ad05c13c
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-10-13 10:20:27 +0200

    formatting

--- Lab2_1.py
+++ Lab2_1.py
@@ -1,16 +1,18 @@
 import sys
 
+
 # sys argv returns the argument to the program
 def main():
-# saves the argument to the variable
+    # saves the argument to the variable
     filename = sys.argv[1]
-    list_of_tuples=read_file(filename)
+    list_of_tuples = read_file(filename)
     user_input = input("Enter a name to search for: ")
     # tuple is an item of the tuple with name,message
     for tuple in list_of_tuples:
         name_in_tuple = tuple[0]
         if name_in_tuple == user_input:
-            display_entry(name_in_tuple, tuple[1])
+            print(display_entry(name_in_tuple, tuple[1]))
+
 
 # function reads the file and
 ## puts data in tuples -- tuple contain name and message
@@ -18,7 +20,8 @@ def read_file(filename):
     try:
         log_file = open(filename, 'r')
     except:
-        exit(f"Error: The file '{filename}' could not be found.")
+        print(f"Error: The file '{filename}' could not be found.")
+        exit()
     list_tuples = []
     end = False
     while not end:
@@ -26,13 +29,14 @@ def read_file(filename):
         if not name:
             end = True
         else:
-            list_tuples.append((name.replace('\n',''),log_file.readline().replace('\n','')))
+            list_tuples.append((name.replace('\n', ''), log_file.readline().replace('\n', '')))
     log_file.close()
     return list_tuples
 
-def display_entry(name,message):
-    print(f"[{name}] --> {message}")
+
+def display_entry(name, message):
+    return f"[{name}] --> {message}"
 
 
-
-main()
+if __name__ == '__main__':
+    main()
--- lab2_3.py
+++ lab2_3.py
@@ -6,9 +6,10 @@ def read_lines(filename):
     try:
         log_file = open(filename, 'r')
     except:
-        exit(f"Error: The file '{filename}' could not be found.")
+        print(f"An error occurred while trying to read the file.")
+        exit()
     end = False
-    list_of_strings= []
+    list_of_strings = []
     while not end:
         name = log_file.readline()
         if not name:
@@ -18,47 +19,43 @@ def read_lines(filename):
     log_file.close()
     return list_of_strings
 
+
 def parse_cars(list_of_strings):
-    cars= []
+    cars = []
     for strings in list_of_strings:
         new_data = strings.split(":")
-        cars.append((new_data[0],int(new_data[1])))
+        cars.append((new_data[0], int(new_data[1])))
     return cars
 
 
 def calculate_percentage(distance, cars):
     percentages = []
     for car in cars:
-        car_percent = math.ceil(float(distance/car[1]*100))
+        car_percent = float(distance / car[1] * 100)
         percentages.append((car[0], car_percent))
     return percentages
 
 
 def display_result(percentages):
-    print("\nTo drive the specified distance would correspond to this many\n"
+    print("\n\nTo drive the specified distance would correspond to this many\n"
           "percent of each cars specified max range.")
     for percentage in percentages:
-        #saving percentage in variable to do if
-        car_percent = percentage [1]
+        # saving percentage in variable to do if
+        car_percent = round(percentage[1])
         if car_percent > 100:
-            print(f"{percentage[0]:34} --> Distance exceeds max range (102%)")
+            print(f"{percentage[0]:36} -->  Distance exceeds max range ({car_percent}%)")
         else:
-            print(f"{percentage[0]:34} --> {car_percent}%")
-
-
-
+            print(f"{percentage[0]:36} -->  {car_percent}%")
 
 
 def main():
-
     filename = sys.argv[1]
-    list_of_strings =read_lines(filename)
-    cars =parse_cars(list_of_strings)
-    input_distance = int(input("How far do you want to drive (kilometers)? "))
-    percentages=calculate_percentage(input_distance,cars)
+    list_of_strings = read_lines(filename)
+    cars = parse_cars(list_of_strings)
+    input_distance = int(input("How far do you want to drive (kilometers)?"))
+    percentages = calculate_percentage(input_distance, cars)
     display_result(percentages)
 
 
-
-main()
-
+if __name__ == '__main__':
+    main()
